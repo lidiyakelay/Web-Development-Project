f9aab872abdb6b747f4be808d5ecf63724873d89
Arsema21 <arsemamulualem19@gmail.com>
2022-12-05 10:37:00 +0300

    login feature

---
+++ Restaurantly/login.php
@@ -0,0 +1,26 @@
+<?php
+include '../bookdb.php';
+if(isset($_POST["login"])){
+  $usernameemail = $_POST["usernameemail"];
+  $password = $_POST["password"];
+  $result = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$usernameemail' OR useremail = '$usernameemail'");
+  $row = mysqli_fetch_assoc($result);
+  if(mysqli_num_rows($result) > 0){
+    $hashpassword= $row['password'];
+    $verified=password_verify($password,$hashpassword);
+    if($verified){
+      $_SESSION["login"] = true;
+      $_SESSION["id"] = $row["id"];
+      header("Location: home.php");
+    }
+    else{
+      echo
+      "<script> alert('Wrong Password or username'); </script>";
+    }
+  }
+  else{
+    echo
+    "<script> alert('User Not Found'); </script>";
+  }
+}
+?>
